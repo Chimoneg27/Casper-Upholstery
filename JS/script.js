fe3f78d4797c21cf56602f3d1b57be5580a0b5b3
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-08-26 23:15:31 +0200

    move services to JS

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,38 @@
 const hamburger = document.querySelector('.burger-menu');
 const navMenu = document.querySelector('.mobile-nav');
+const rootServices = document.getElementById('cards');
+
+const services = [
+  {
+    title: "Re-upholstery",
+    image: "./images/serve1.jpg",
+    altText: "sofa",
+    description: "Our re-upholstery services will give your furniture a new look and feel."
+  }
+]
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
-document.querySelector('.page-links').forEach((n) => n.addEventListener('click', () => {
+document.querySelectorAll('.page-links').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+
+
+const generateCards = (arr, box) => {
+  for(let i = 0; i < arr.length; i++) {
+    box.innerHTML += `
+      <div class="offered">
+        <h3>${arr[i].title}</h3>
+        <img src="${arr[i].image}" alt="${arr[i].altText}" />
+        <p>${arr[i].description}</p>
+      </div>
+    `
+  }
+}
+
+generateCards(services, rootServices);
